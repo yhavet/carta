298177325d23ba4f45101ebe1fef86bf622a392e
Yhavet Alaniz <104223938+yhavet@users.noreply.github.com>
2026-02-10 00:34:56 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 let heartInterval;
+let currentIndex = 0; 
 
 function openEnvelope() {
     const envelope = document.querySelector('.envelope');
     const overlay = document.getElementById('photo-overlay');
-    
     envelope.classList.add('open');
-    
     setTimeout(() => {
         overlay.classList.add('visible');
         startHearts();
@@ -16,36 +15,52 @@ function closeEnvelope() {
     const envelope = document.querySelector('.envelope');
     const overlay = document.getElementById('photo-overlay');
     overlay.classList.remove('visible');
-    stopHearts(); 
+    stopHearts();
     setTimeout(() => {
         envelope.classList.remove('open');
+
+        currentIndex = 0;
+        updateCarousel();
     }, 300);
 }
 
-// --- LÓGICA DE CORAZONES ---
+
+function moveSlide(direction) {
+    const track = document.getElementById('track');
+    const slides = document.querySelectorAll('.carousel-slide');
+    const totalSlides = slides.length;
+
+
+    currentIndex += direction;
+
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    updateCarousel();
+}
+
+function updateCarousel() {
+    const track = document.getElementById('track');
+  
+    track.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
 
 function createHeart() {
     const container = document.getElementById('hearts-container');
-    
     if (!container) return;
-
     const heart = document.createElement('div');
     heart.classList.add('floating-heart');
     heart.innerHTML = '❤️'; 
-    
     heart.style.left = Math.random() * 100 + 'vw';
-    
     const size = Math.random() * 20 + 20; 
     heart.style.fontSize = size + 'px';
-    
     heart.style.animationDuration = Math.random() * 2 + 3 + 's'; 
-    
     container.appendChild(heart);
-
-    // Eliminar el corazón del HTML después de 5 segundos para no saturar la memoria
-    setTimeout(() => {
-        heart.remove();
-    }, 5000);
+    setTimeout(() => { heart.remove(); }, 5000);
 }
 
 function startHearts() {
@@ -55,7 +70,5 @@ function startHearts() {
 function stopHearts() {
     clearInterval(heartInterval);
     const container = document.getElementById('hearts-container');
-    if (container) {
-        container.innerHTML = '';
-    }
+    if (container) container.innerHTML = '';
 }
